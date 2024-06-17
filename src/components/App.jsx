7ded2a59045dcdabf4450d7e05d037623ae7922a
Joseph Camacho <spongkj@gmail.com>
2024-06-17 18:22:23 +0800

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,34 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      this.setState({contacts: JSON.parse(savedContacts)});
+    } 
+    // else {
+    //   this.setState({
+    //     contacts: [
+    //       {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    //       {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    //       {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    //       {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    //     ],
+    //     filter: '',
+    //   });
+    //   console.log("state has change therefore componentDidUpdate will be called");
+    // }
+    console.log("componentDidMount");
+  };
+
+  componentDidUpdate(_prevProps, prevState) {
+    const {contacts} = this.state;
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+    console.log("componentDidUpdate");
+  };
+
   addContact = newContact => {
     this.setState(prevState =>({
       contacts: [...prevState.contacts, newContact],
@@ -41,6 +69,7 @@ export class App extends Component {
   };
 
   render() {
+    console.log("render is called");
     const {contacts, filter} = this.state;
     return (
     <div>
--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,22 +1,54 @@
+import { Component } from 'react';
 import css from './ContactListItem.module.css';
 import PropTypes from 'prop-types';
 
-export const ContactListItem = ({ filteredContact, deleteContact }) => {
+export class ContactListItem extends Component {
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  };
+
   // handleDelete method
-  const handleDelete = () => {
+  handleDelete = () => {
+    const {filteredContact, deleteContact} = this.props;
     deleteContact(filteredContact.id);
   };
 
-  return (
-    <li className={css.contactListItem}>
-      <p>{filteredContact.name}:</p>
-      <p>{filteredContact.number}</p>
-      <button onClick={handleDelete}>Delete</button>
-    </li>
-  );
-};
+  render () {
+    const {filteredContact} = this.props;
+    return (
+      <li className={css.contactListItem}>
+        <p>{filteredContact.name}:</p>
+        <p>{filteredContact.number}</p>
+        <button onClick={this.handleDelete}>Delete</button>
+      </li>
+    );
+}
+}
 
-ContactListItem.propTypes = {
-  filteredContact: PropTypes.object.isRequired,
-  deleteContact: PropTypes.func.isRequired,
-};
+// ContactListItem.propTypes = {
+//   filteredContact: PropTypes.object.isRequired,
+//   deleteContact: PropTypes.func.isRequired,
+// };
+
+
+// Functional Components
+// export const ContactListItem = ({ filteredContact, deleteContact }) => {
+//   // handleDelete method
+//   const handleDelete = () => {
+//     deleteContact(filteredContact.id);
+//   };
+
+//   return (
+//     <li className={css.contactListItem}>
+//       <p>{filteredContact.name}:</p>
+//       <p>{filteredContact.number}</p>
+//       <button onClick={handleDelete}>Delete</button>
+//     </li>
+//   );
+// };
+
+// ContactListItem.propTypes = {
+//   filteredContact: PropTypes.object.isRequired,
+//   deleteContact: PropTypes.func.isRequired,
+// };
